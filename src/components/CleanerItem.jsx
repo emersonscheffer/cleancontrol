78a9cfc8d2474abb64cf52b783d85d120c6c0dd6
emersonscheffer <32528159+emersonscheffer@users.noreply.github.com>
2026-03-25 01:42:31 -0700

    Update CleanerItem.jsx

--- src/components/CleanerItem.jsx
+++ src/components/CleanerItem.jsx
@@ -7,23 +7,38 @@ const CleanerItem = ({ cleaner, deleteCleaner }) => {
   return (
     <div
       style={{
-        border: "1px solid #ccc",
-        borderRadius: "8px",
-        padding: "10px",
-        marginBottom: "10px",
+        // border: "1px solid #ccc",
+        borderRadius: "10px",
+        // padding: "10px",
+        // marginBottom: "10px",
+        backgroundColor: "#F5F5F5",
+        display: "grid",
+        gridTemplateAreas: `
+        ". . . ." 
+        ". name . wallet" 
+          ". . . ." 
+        `,
+        gridTemplateRows: "10px 1fr 10px",
+        gridTemplateColumns: "10px 1fr 1fr 60px",
+        // alignItems: "center",
+        // justifyContent: "space-between",
+        width: "270px",
+        height: "60px",
+
+        
       }}
     >
-      <h3>{cleaner.name}</h3>
-      <h3>Wallet: ${cleaner.wallet}</h3>
+      <h3 style={{ gridArea: "name" }}>{cleaner.name}</h3>
+      <h3 style={{ gridArea: "wallet", color: "red" }}>$ {cleaner.wallet}</h3>
 
-      <button
+      {/* <button
         style={{ backgroundColor: "red", color: "white" }}
         onClick={() => {
           deleteCleaner(cleaner.id);
         }}
       >
         Delete
-      </button>
+      </button> */}
     </div>
   );
 };
